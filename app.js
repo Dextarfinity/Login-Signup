141af715d2cf709c0696d592065642f09648ad8f
dextarfinity <xdfeverharsh@gmail.com>
2024-09-15 13:27:23 +0800

    last

--- app.js
+++ app.js
@@ -1,17 +1,13 @@
 window.addEventListener('load', function() {
   const loader = document.getElementById('loader');
   
-  // Keep loader visible for 3 seconds
   setTimeout(() => {
-    loader.style.opacity = '0'; // Start fading out
-    loader.style.transition = 'opacity 1s ease'; // Add transition for the fade-out effect
-
-    // After the fade-out, set display to 'none' to hide the loader completely
+    loader.style.opacity = '0'; 
+    loader.style.transition = 'opacity 1s ease'; 
     setTimeout(() => {
       loader.style.display = 'none';
-    }, 1000); // Delay to match the fade-out duration (1 second)
-    
-  }, 3000); // 3000 milliseconds = 3 seconds
+    }, 1000);
+  }, 3000); 
 });
 
 
